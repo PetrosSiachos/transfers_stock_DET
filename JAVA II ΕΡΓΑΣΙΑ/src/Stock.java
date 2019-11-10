8216e9593a9a7edd30c1d389d47c52407dea1b30
ppetropoulos <56507562+ppetropoulos@users.noreply.github.com>
2019-11-10 17:36:45 +0200

    Update Stock.java
    TRY 2

--- JAVA II ΕΡΓΑΣΙΑ/src/Stock.java
+++ JAVA II ΕΡΓΑΣΙΑ/src/Stock.java
@@ -1,48 +1,45 @@
 import java.util.ArrayList;
-
 public class Stock {
-
-	int id; //id αυτοαυξανόμενο
+	int id; //id Ξ±Ο…Ο„ΞΏΞ±Ο…ΞΎΞ±Ξ½ΟΞΌΞµΞ½ΞΏ
 	static int counter = 1;
 	double price;
 	int quantity;
 	static ArrayList<Stock> stocks = new ArrayList<Stock>(); // an arrayList of all stocks
 	int minQuantity; // minimun quantity of each product or stock
 	
-	//constructor that constructs a stock for Stock
+//constructor that constructs a stock for Stock
 	public Stock(double price, int quantity, int minQuantity) {
 		super();
 		this.id = counter++;
 		this.price = price;
 		this.quantity = quantity;
 		this.minQuantity = minQuantity;
-		if (this.quantity < minQuantity) {
-			this.quantity = minQuantity;
-		}// if quantity given is smaller than minQuantity then quantity=minQuantity!
+		checkMinQuantity();
+		// if quantity given is smaller than minQuantity then quantity=minQuantity!
 		// In order to create a stock we are going to set quantity to a specific amount = minQuantity
 		//LATER, IF IT GETS LOWER THAN MIN WE MAKE A PUCHASE AND USE METHOD "checkMinQuantity"!!!!! 
 		stocks.add(this);
 	}
 	
-	
-	//constructor that constructs a stock for Order
-	public Stock(int id, double price, int quantity, int minQuantity) {
-		super();
-		this.id = id;
-		this.price = price;
-		this.quantity = quantity;
-		this.minQuantity = minQuantity;
-	}
-
-
-	//prints all elements of ArrayList stocks
+//constructor that constructs a stock for Order
+		public Stock(int id, double price, int quantity, int minQuantity) {
+			super();
+			this.id = id;
+			this.price = price;
+			this.quantity = quantity;
+			this.minQuantity = minQuantity;
+			checkMinQuantity();
+		}
+//we have a method that checks if quantity is smaller than minQuantity.Is this situation, quantity=minQuantity
+		
+//prints all elements of ArrayList stocks
 	public static void printAllStocks() {
 		for (int i=0; i<stocks.size(); i++) {
 			System.out.println(stocks.get(i));
 		}
 	}
 	
-	//prints the highest quantity of the stock and the id to which the quantity belongs to
+//prints the highest quantity of the stock and the id to which the quantity belongs to
 	public static void printHighestQuantity() {
 		int max = 0;
 		int id = 0;
@@ -55,7 +52,7 @@ public class Stock {
 		System.out.println("The highest quantity of all stocks is " + max + " and belongs to the product with id " + id);
 	}
 	
-	//prints the lowest quantity of the stock and the id to which the quantity belongs to
+//prints the lowest quantity of the stock and the id to which the quantity belongs to
 	public static void printLowestQuantity() {
 		int min = 1000000000;
 		int id = 0;
@@ -68,7 +65,7 @@ public class Stock {
 		System.out.println("The lowest quantity of all stocks is " + min + " and belongs to the product with id " + id);
 	}
 	
-	// checks if quantity given is smaller than minQuantity then sets quantity=minQuantity!
+// checks if quantity given is smaller than minQuantity then sets quantity=minQuantity!
 	public void checkMinQuantity() {
 		if (this.quantity < minQuantity) {
 			this.quantity = minQuantity;
@@ -80,7 +77,4 @@ public class Stock {
 	public String toString() {
 		return "Stock [id=" + id + ", price=" + price + ", quantity=" + quantity + ", minQuantity=" + minQuantity + "]";
 	}
-	
-	
-		
 }
