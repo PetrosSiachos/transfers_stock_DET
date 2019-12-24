0556337c74d08a63b6fc9cc5abbe23e81a20778a
PanagiotaSargenti <56512834+PanagiotaSargenti@users.noreply.github.com>
2019-12-24 12:19:44 +0200

    Update Statistics.java

--- Ergasia2a/src/Statistics.java
+++ Ergasia2a/src/Statistics.java
@@ -1,19 +1,7 @@
 import java.util.ArrayList;
 public class Statistics {
-	public static ArrayList<Statistics> statistics = new ArrayList<Statistics>();
-	double countofvalues;
-	double averageofvalues;
-	double max;
-	double min;
 	
-	public Statistics() {
-		super();
-		this.countofvalues = Order.priceofgoodorders;
-		this.averageofvalues = Order.priceofgoodorders/(Order.numberofgoodorders);
-		this.min = Minprice();
-		this.max = Maxprice();
-	}
-			
+	/* Ελάχιστη τιμή παραγγελίας*/
 	public static double Minprice() {
 		int id;
 		double min = 1000000000;
@@ -26,6 +14,7 @@ public class Statistics {
 		return min;
 	}
 	
+	/* Μέγιστη τιμή παραγγελίας */
 	public static double Maxprice() {
 		int id;
 		double max = -1;
@@ -36,6 +25,120 @@ public class Statistics {
 			}
 		}
 		return max;
+	}
+	
+	/* Ποσοστό επιτυχημένων παραγγελιών */
+	public static double rateofgoodorders() {
+		double rate;
+		rate = Order.numberofgoodorders/Order.numberoforders;
+		return rate;
+	}
+	
+	/* Ποσοστό αποτυχημένων παραγγελιών */
+	public static double rateofblackorders() {
+		double rate;
+		rate = Order.numberofBlackList/Order.numberoforders;
+		return rate;
+	}
+	
+	/* Δημοφιλές προϊόν (αυτό που υπάρχει στις περισσότερες παραγγελίες */
+	static int k[] = new int [Order.numberofproducts];
+	public static int popularpr() {
+		int id = 0;
+		int cust;
+		for (int i = 0; i < Order.orders.size(); i++) {
+			for (int j = 0; j < Order.numberofproducts; j++ ) {
+				if (Order.orders.get(i).order.get(i).id == Order.orders.get(j).idproducts.get(j)) {
+					k[j] = k[j] + 1;
+				}
+			}
+		}
+		int max = -1;
+		for (int s = 0; s < Order.numberofproducts; s++) {
+			if (k[s] > max) {
+				max = k[s];
+				id = k[s];
+				cust = Order.orders.get(id).customer;
+			}
+		}
+		return id;
+	}
+	
+	/* ποσοστά προϊόντων στις παραγγελίες */
+	public static double[] average_pr_orders() {
+		double i[] = new double [Order.numberofproducts];
+		for (int s = 0; s < Order.numberofproducts; s++) {
+			i[s] = k[s]/Order.numberofproducts;
+		}
+		return i;
+	}
+	
+	public static int popcustomer() {
+		int idcust;
+		int a[] = new int[Customer.customers.size()];
+		for (int i=0; i < Order.orders.size(); i++) {
+			for (int j = 0; j < Customer.customers.size(); j++) {
+				if (Order.orders.get(i).idcust == Customer.customers.get(j).custid) {
+					a[j] = a[j] + 1;
+				}
+			}
+		}
+		int max = -1;
+		for (int s = 0; s < a.length; s++) {
+			if (a[s] > max) {
+				max = a[s];
+				idcust = Customer.customers.get(s).custid;
+			}
+		}
+		return idcust;
+	}
+	
+	
+	/* Μέγιστη ποσότητα προϊόντων */
+	public static int Highestquantity() {
+		int highq = 0;
+		int id = 0;
+		for (int i = 0; i < Stock.stocks.size(); i++) {
+			if (Stock.stocks.get(i).quantity > highq) {
+				highq = Stock.stocks.get(i).quantity;
+				id = Stock.stocks.get(i).id;
+			}
+		}
+		return highq;
+	}
+	
+	/* Ελάχιστη ποσότητα προϊόντων */
+	public static int Lowestquantity() {
+		int lowq = 1000000000;
+		int id = 0;
+		for (int i = 0; i < Stock.stocks.size(); i++) {
+			if (Stock.stocks.get(i).quantity < lowq) {
+				lowq= Stock.stocks.get(i).quantity;
+				id = Stock.stocks.get(i).id;
+			}
+		}
+		return lowq;
+		
+	}
+	
+	public static double minprice() {
+		double minp = 10000000;
+		for (int i=0; i < Stock.stocks.size(); i++) {
+			if (Stock.stocks.get(i).price < minp) {
+				minp = Stock.stocks.get(i).price;
+			}
+		}
+		return minp;
+	}
+	
+	public static double maxprice() {
+		double maxp = 0;
+		for (int i=0; i < Stock.stocks.size(); i++) {
+			if (Stock.stocks.get(i).price > maxp) {
+				maxp = Stock.stocks.get(i).price;
+			}
+		}
+		return maxp;
 	}
 	
 }
