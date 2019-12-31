d695f0ca53d6b9486868cf14bf1b7cc5f4b5e3a4
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2019-12-31 14:05:21 +0200

    1) AverageCreditworthiness of Customers

--- JAVA II xoris grafiki diepafi/src/Statistics.java
+++ JAVA II xoris grafiki diepafi/src/Statistics.java
@@ -1,1 +1,16 @@
-public class Statistics {}
+public class Statistics {
+  
+/*-------------------Statistics of Class Customer-------------------------*/
+	
+	/* 1) AverageCreditworthiness of Customers|| μέση πιστολιπτική ικανότητα πελατών*/
+	public static double AverageCreditworthiness() {
+		int sum = 0;
+		for (Customer cust : Customer.customers) {
+			sum = sum + cust.getCreditworthiness();
+		}
+		return (double) sum/Customer.customers.size();
+
+
+
+
+}
