c58c9fcfd44625d7cdac8f80a8278244661f1fd8
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2019-12-31 14:06:43 +0200

    Customer with max creditworthiness

--- JAVA II xoris grafiki diepafi/src/Statistics.java
+++ JAVA II xoris grafiki diepafi/src/Statistics.java
@@ -2,15 +2,33 @@ public class Statistics {
   
 /*-------------------Statistics of Class Customer-------------------------*/
 	
-	/* 1) AverageCreditworthiness of Customers|| μέση πιστολιπτική ικανότητα πελατών*/
+/* 1) AverageCreditworthiness of Customers|| μέση πιστολιπτική ικανότητα πελατών*/
 	public static double AverageCreditworthiness() {
 		int sum = 0;
 		for (Customer cust : Customer.customers) {
 			sum = sum + cust.getCreditworthiness();
 		}
 		return (double) sum/Customer.customers.size();
-
-
+	}
+	
+/* 2) Customer with max creditworthiness || πελάτης με μέγιστη πιστολιπτική ικανότητα*/
+	public static int[] CustomerMaxCreaditworthiness() {
+		int cred = 0;
+		int i = 0; 
+		int [] array = new int [Customer.customers.size()];
+		for (Customer cust : Customer.customers) {
+			if (cust.getCreditworthiness() > cred) {
+				cred = cust.getCreditworthiness();
+			}
+		}
+		for (Customer cust : Customer.customers) {
+			if (cust.getCreditworthiness() == cred) {
+				array[i] = cust.getIdcustomer();
+				i++;
+			}
+		}
+		return array;
+	}
 
 
 }
