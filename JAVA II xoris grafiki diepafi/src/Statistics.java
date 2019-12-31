5216d41dc2516fa895bd4dfea5aaeafb08e2ede7
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2019-12-31 14:32:49 +0200

    24) Customer with min Orders

--- JAVA II xoris grafiki diepafi/src/Statistics.java
+++ JAVA II xoris grafiki diepafi/src/Statistics.java
@@ -234,6 +234,7 @@ public class Statistics {
 		}
 		return array;
 	}
+	
 /* 23) Customer with max Orders || πελάτης με τις περισσότερες παραγγελίες*/
 	public static int[] CustomerMaxOrders() {
 		int max = 0;
@@ -255,6 +256,29 @@ public class Statistics {
 		}
 		return array;
 	}
+/* 24) Customer with min Orders || πελάτης με τις λιγότερες παραγγελίες*/
+	public static int[] CustomerMinOrders() {
+		int min = 0;
+		int k = 0 ;
+		int [] arr = CustomerSumOrders();
+		int [] array = new int [Customer.customers.size()];
+		for(int i = 0 ; i < arr.length ; i++) {
+			if (arr[i] < min) {
+				min = arr[i];
+			}
+		}
+		
+		for (Customer cust : Customer.customers) {
+			int idCust = cust.getIdcustomer();
+			if (arr[idCust -2] == min) {
+				array[k] = cust.getIdcustomer();
+				k++;
+			}
+		}
+		return array;
+	}
+	
+	
 	
 	
 				
