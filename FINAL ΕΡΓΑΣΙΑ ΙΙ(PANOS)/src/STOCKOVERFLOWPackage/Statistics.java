d96a7828181c0975bde92f680f02f26393f0c170
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2020-01-02 12:45:26 +0200

    22) CustomerSumOrders

--- FINAL ΕΡΓΑΣΙΑ ΙΙ(PANOS)/src/STOCKOVERFLOWPackage/Statistics.java
+++ FINAL ΕΡΓΑΣΙΑ ΙΙ(PANOS)/src/STOCKOVERFLOWPackage/Statistics.java
@@ -1,5 +1,20 @@
 package STOCKOVERFLOWPackage;
 
 public class Statistics {
+  /*-------------------Statistics of Class Order-------------------------*/
+	
+	/* 22) CustomerSumOrders || άθροισμα παραγγελιών κάθε πελάτη */
+	public static int[] CustomerSumOrders() throws Exception {
+		int [] array = new int [Customer.getCustomers().size()];
+		for (int i  = 0 ; i < array.length ; i++) {
+			array[i] = 0 ;
+		}
+		for(Order o : Order.getOrders()) {
+			int idCust = o.getIdcustomer();
+			array[idCust -2]++; 
+		}
+		return array;
+	}
+	
 
 }
