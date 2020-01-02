74ac3024d69b7dcf0e92776fc5c0b677a376aa54
ElenaTzerefou <56511251+ElenaTzerefou@users.noreply.github.com>
2020-01-02 14:58:45 +0200

    onomata return stous customers

--- FINAL ΕΡΓΑΣΙΑ ΙΙ(PANOS)/src/STOCKOVERFLOWPackage/Statistics.java
+++ FINAL ΕΡΓΑΣΙΑ ΙΙ(PANOS)/src/STOCKOVERFLOWPackage/Statistics.java
@@ -1,9 +1,22 @@
-package STOCKOVERFLOWPackage;
 
 public class Statistics {
-  /*-------------------Statistics of Class Order-------------------------*/
 	
-	/* 22) CustomerSumOrders || athroisma paraggelion kathe pelati */
+	/*---------------------------------------------------------------------*/
+	
+	/* Ποσοστό επιτυχημένων παραγγελιών */
+	public static double rateofgoodorders() throws Exception {
+		double rate;
+		System.out.println(Order.getGoodOrders().size());
+		System.out.println(Order.getOrders().size());
+		rate = ((double)Order.getGoodOrders().size())/((double)Order.getOrders().size());
+		return rate;
+	}
+	
+
+	
+	/*-------------------Statistics of Class Order-------------------------*/
+	
+	/* 22) CustomerSumOrders || άθροισμα παραγγελιών κάθε πελάτη */
 	public static int[] CustomerSumOrders() throws Exception {
 		int [] array = new int [Customer.getCustomers().size()];
 		for (int i  = 0 ; i < array.length ; i++) {
@@ -17,12 +30,12 @@ public class Statistics {
 	}
 	
 	
-	/* 23) Customer with max Orders || pelatis me perissoteres paraggelies*/
-	public static int[] CustomerMaxOrders() throws Exception {
+	/* 23) Customer with max Orders || πελάτης με τις περισσότερες παραγγελίες*/
+	public static String[] CustomerMaxOrders() throws Exception {
 		int max = 0;
 		int k = 0 ;
 		int [] arr = CustomerSumOrders();
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for(int i = 0 ; i < arr.length ; i++) {
 			if (arr[i] > max) {
 				max = arr[i];
@@ -32,7 +45,7 @@ public class Statistics {
 		for (Customer cust : Customer.getCustomers()) {
 			int idCust = cust.getIdcustomer();
 			if (arr[idCust -2] == max) {
-				array[k] = cust.getIdcustomer();
+				array[k] = cust.getNamecustomer();
 				k++;
 			}
 		}
@@ -40,12 +53,12 @@ public class Statistics {
 	}
 	
 	 	
-	/* 24) Customer with min Orders || pelatis me ligoteres paraggelies*/
-	public static int[] CustomerMinOrders() throws Exception {
+	/* 24) Customer with min Orders || πελάτης με τις λιγότερες παραγγελίες*/
+	public static String[] CustomerMinOrders() throws Exception {
 		int min = 0;
 		int k = 0 ;
 		int [] arr = CustomerSumOrders();
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for(int i = 0 ; i < arr.length ; i++) {
 			if (arr[i] < min) {
 				min = arr[i];
@@ -55,7 +68,7 @@ public class Statistics {
 		for (Customer cust : Customer.getCustomers()) {
 			int idCust = cust.getIdcustomer();
 			if (arr[idCust -2] == min) {
-				array[k] = cust.getIdcustomer();
+				array[k] = cust.getNamecustomer();
 				k++;
 			}
 		}
@@ -66,7 +79,7 @@ public class Statistics {
 	
 	 /*-------------------Statistics of Class Customer-------------------------*/
 	
-	/* 1) AverageCreditworthiness of Customers|| mesi pistoliptiki ikanotita */
+	/* 1) AverageCreditworthiness of Customers|| μέση πιστολιπτική ικανότητα πελατών*/
 	public static double AverageCreditworthiness() throws Exception {
 		int sum = 0;
 		for (Customer cust : Customer.getCustomers()) {
@@ -75,12 +88,11 @@ public class Statistics {
 		return (double) sum/Customer.getCustomers().size();
 	}
 	
-	
-	 /*2) Customer with max creditworthiness || pelatis me megisti pistoliptiki ikanotita*/
-	public static int[] CustomerMaxCreaditworthiness() throws Exception {
+	 /*2) Customer with max creditworthiness || πελάτης με μέγιστη πιστολιπτική ικανότητα*/
+	public static String[] CustomerMaxCreaditworthiness() throws Exception {
 		int cred = 0;
 		int i = 0; 
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getCreditworthiness() > cred) {
 				cred = cust.getCreditworthiness();
@@ -88,7 +100,7 @@ public class Statistics {
 		}
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getCreditworthiness() == cred) {
-				array[i] = cust.getIdcustomer();
+				array[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
@@ -96,12 +108,11 @@ public class Statistics {
 	}
 	
 	
-	
-	/* 3) Customer with min creditworthiness || pelatis me elaxisti pistoliptiki ikanotita*/
-	public static int[] CustomerMinCreaditworthiness() throws Exception {
+	/* 3) Customer with min creditworthiness || πελάτης με ελάχιστη πιστολιπτική ικανότητα*/
+	public static String[] CustomerMinCreaditworthiness() throws Exception {
 		int cred = 0;
 		int i = 0; 
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getCreditworthiness() < cred) {
 				cred = cust.getCreditworthiness();
@@ -109,7 +120,7 @@ public class Statistics {
 		}
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getCreditworthiness() == cred) {
-				array[i] = cust.getIdcustomer();
+				array[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
@@ -117,7 +128,7 @@ public class Statistics {
 	}
 	
 	
-	 /*4) Satisfactory average creditworthiness or not || ikanopoiitiki pistoliptiki ikanotita i oxi*/
+	 /*4) Satisfactory average creditworthiness or not || ικανοποιητική μέση πιστολιπτική ικανότητα ή όχι?*/
 	public static boolean SatisfactoryAverageCreditworthiness() throws Exception {
 		boolean sat = false;
 		if (Statistics.AverageCreditworthiness() > 70) {
@@ -126,8 +137,7 @@ public class Statistics {
 		return sat;
 	}
 	
-		
-	 /*5) AverageYears_of_cooperation of Customers|| mesi timi xronon sunergasias*/
+	 /*5) AverageYears_of_cooperation of Customers|| μέση τιμή χρόνων συνεργασίας με πελάτες*/
 	public static double AverageYears_of_cooperation() throws Exception {
 		int sum = 0;
 		for (Customer cust : Customer.getCustomers()) {
@@ -136,12 +146,11 @@ public class Statistics {
 		return (double) sum/Customer.getCustomers().size();
 	}
 	
-		
-	/*6) Customer with max Years_of_cooperation || pelatis me megista xronia sunergasias*/
-	public static int[] CustomerMaxYears_of_cooperation() throws Exception {
+	/*6) Customer with max Years_of_cooperation || πελάτης με μέγιστη τιμή χρόνων συνεργασίας*/
+	public static String[] CustomerMaxYears_of_cooperation() throws Exception {
 		int years = 0;
 		int i = 0; 
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getYears_of_cooperation() > years) {
 				years = cust.getYears_of_cooperation();
@@ -149,7 +158,7 @@ public class Statistics {
 		}
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getYears_of_cooperation() == years) {
-				array[i] = cust.getIdcustomer();
+				array[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
@@ -157,12 +166,11 @@ public class Statistics {
 	}
 	
 	
-	
-	/* 7) Customer with min Years_of_cooperation || pelatis me elaxista xronia sunergasias*/
-	public static int[] CustomerMinYears_of_cooperation() throws Exception {
+	/* 7) Customer with min Years_of_cooperation || πελάτης με ελάχιστη τιμή χρόνων συνεργασίας*/
+	public static String[] CustomerMinYears_of_cooperation() throws Exception {
 		int years = 100;
 		int i = 0; 
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getYears_of_cooperation() < years) {
 				years = cust.getYears_of_cooperation();
@@ -170,15 +178,14 @@ public class Statistics {
 		}
 		for (Customer cust : Customer.getCustomers()) {
 			if (cust.getYears_of_cooperation() == years) {
-				array[i] = cust.getIdcustomer();
+				array[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
 		return array;
 	}
 
-
-	/* 8) AveragePercent_of_stock of Customers|| meso pososton metoxon*/
+	/* 8) AveragePercent_of_stock of Customers|| μέσο ποσοστό μετοχών*/
 	public static double AveragePercent_of_stock() throws Exception {
 		double sum = 0;
 		for (Customer cust : Customer.getCustomers()) {
@@ -187,13 +194,11 @@ public class Statistics {
 		return (double) sum/Customer.getCustomers().size();
 	}
 	
-	
- 	
-	/* 9) Customer with max Percent_of_stock || pelatis me perissoteres metoxes*/
-	public static int[] CustomerMaxPercent_of_stock() throws Exception {
+	/* 9) Customer with max Percent_of_stock || πελάτης με τις περισσότερες μετοχές*/
+	public static String[] CustomerMaxPercent_of_stock() throws Exception {
 		double stock = 0;
 		int i = 0; 
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for (Customer cust : Customer.getCustomers()) {
 			if ((double)cust.getPercent_of_stock() > stock) {
 				stock = (double) cust.getPercent_of_stock();
@@ -201,19 +206,18 @@ public class Statistics {
 		}
 		for (Customer cust : Customer.getCustomers()) {
 			if ((double)cust.getPercent_of_stock() == stock) {
-				array[i] = cust.getIdcustomer();
+				array[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
 		return array;
 	}
 	
-	
-	 /*10) Customer with min Percent_of_stock || pelatis me ligoteres metoxes*/
-	public static int[] CustomerMinPercent_of_stock() throws Exception {
+	 /*10) Customer with min Percent_of_stock || πελάτης με τις λιγότερες μετοχές*/
+	public static String[] CustomerMinPercent_of_stock() throws Exception {
 		double stock = 1000.0;
 		int i = 0; 
-		int [] array = new int [Customer.getCustomers().size()];
+		String [] array = new String [Customer.getCustomers().size()];
 		for (Customer cust : Customer.getCustomers()) {
 			if ((double)cust.getPercent_of_stock() < stock) {
 				stock = (double) cust.getPercent_of_stock();
@@ -221,14 +225,14 @@ public class Statistics {
 		}
 		for (Customer cust : Customer.getCustomers()) {
 			if ((double)cust.getPercent_of_stock() == stock) {
-				array[i] = cust.getIdcustomer();
+				array[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
 		return array;
 	}
 	
-	 /*11) Array of Value of customers || lista me aksia pelaton*/
+	 /*11) Array of Value of customers || λίστα με αξία πελατών*/
 	public static double[] ValueOfCustomers() throws Exception {
 		int i = 0;
 		double [] array = new double [Customer.getCustomers().size()];
@@ -241,13 +245,13 @@ public class Statistics {
 		}
 		return array;
 	}
-		
-	 /*12) Customer with max value || pelatis me megaluteri aksia*/
-	public static double[] CustomerMostValuable() throws Exception {
+	
+	 /*12) Customer with max value || πιο χρήσιμος πελάτης*/
+	public static String[] CustomerMostValuable() throws Exception {
 		double MaxValue = 0;
 		int i = 0; 
 		double [] array = Statistics.ValueOfCustomers();
-		double [] array2 = new double [Customer.getCustomers().size()];
+		String [] array2 = new String [Customer.getCustomers().size()];
 		for (int k = 0 ; k < array.length ; k ++) {
 			if (array[k] > MaxValue) {
 				MaxValue = array[k];
@@ -257,20 +261,19 @@ public class Statistics {
 		for (Customer cust : Customer.getCustomers()) {
 			int id = cust.getIdcustomer();
 			if (array[id - 2] == MaxValue) {
-				array2[i] = cust.getIdcustomer();
+				array2[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
 		return array2;
 	}
 	
-		
-	 /*13) Customer with min value || pelatis me mikroteri aksia*/
-	public static double[] CustomerLessValuable() throws Exception {
+	 /*13) Customer with min value || λιγότερο χρήσιμος πελάτης*/
+	public static String[] CustomerLessValuable() throws Exception {
 		double MinValue = 0;
 		int i = 0; 
 		double [] array = Statistics.ValueOfCustomers();
-		double [] array2 = new double [Customer.getCustomers().size()];
+		String [] array2 = new String [Customer.getCustomers().size()];
 		for (int k = 0 ; k < array.length ; k ++) {
 			if (array[k] < MinValue ) {
 				MinValue = array[k];
@@ -280,15 +283,14 @@ public class Statistics {
 		for (Customer cust : Customer.getCustomers()) {
 			int id = cust.getIdcustomer();
 			if (array[id - 2] == MinValue) {
-				array2[i] = cust.getIdcustomer();
+				array2[i] = cust.getNamecustomer();
 				i++;
 			}
 		}
 		return array2;
 	}
 	
-	
-	/* 14) HealthyDoingBusiness: if 60% of customers have creditworthiness above 60 then we are ok/else not || an to 60% ton pelaton exoun pistoliptiki ikanotita pano apo 60 eimaste kala*/
+	/* 14) HealthyDoingBusiness: if 60% of customers have creditworthiness above 0.6 then we are ok/else not || αν το 60% των πελατών έχουν πιστοληπτική ικανότητα πάνω από 0.6 είμαστε καλά*/
 	public static boolean HealthyDoingBusiness() throws Exception {
 		int count = 0;
 		for(Customer cust : Customer.getCustomers()) {
@@ -303,8 +305,5 @@ public class Statistics {
 		return false;
 	}
 	
-	  /*------------------------------------------------------------------*/
-	
-	 	
-
 }
+
