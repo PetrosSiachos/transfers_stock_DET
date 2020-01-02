ad563efd9a44596696616bb025dc5909140ca9f2
PanagiotaSargenti <56512834+PanagiotaSargenti@users.noreply.github.com>
2020-01-02 12:53:45 +0200

    Update Statistics.java

--- Ergasia2a/src/Statistics.java
+++ Ergasia2a/src/Statistics.java
@@ -1,54 +1,42 @@
 package STOCKOVERFLOWPackage;
 
 public class Statistics {
-
-	/* Ελάχιστη τιμή παραγγελίας */
-	public static double Minprice_orders() throws Exception {
-		int id;
-		double min = 1000000000;
-		for (int i = 0; i < Order.getGoodOrders().size(); i++) {
-			if (Order.getGoodOrders().get(i).price1 < min) {
-				min= Order.getGoodOrders().get(i).price1;
-			}
-		}
-		return min;
-	}
-	
-	/* Μέγιστη τιμή παραγγελίας */
-	public static double Maxprice_orders() throws Exception {
-		int id;
-		double max = -1;
-		for (int i = 0; i < Order.getGoodOrders().size(); i++) {
-			if (Order.getGoodOrders().get(i).price1 > max) {
-				max= Order.getGoodOrders().get(i).price1;
-			}
-		}
-		return max;
-	}
 	
 	/* Ποσοστό επιτυχημένων παραγγελιών */
 	public static double rateofgoodorders() throws Exception {
 		double rate;
-		System.out.println(Order.GoodOrders.size());
-		System.out.println(Order.orders.size());
-		rate = ((double)Order.GoodOrders.size())/((double)Order.orders.size());
+		System.out.println(Order.getGoodOrders().size());
+		System.out.println(Order.getOrders().size());
+		rate = ((double)Order.getGoodOrders().size())/((double)Order.getOrders().size());
 		return rate;
 	}
 	
 	/* Ποσοστό αποτυχημένων παραγγελιών */
 	public static double rateofblackorders() throws Exception {
 		double rate;
-		rate = ((double)Order.BlackList.size())/((double)Order.orders.size());
+		rate = ((double)Order.getBlackList().size())/((double)Order.getOrders().size());
 		return rate;
 	}
 	
 	/* Δημοφιλές προϊόν (αυτό που υπάρχει στις περισσότερες παραγγελίες */
-	public static int popularpr() throws Exception {
-		int k[] = new int [Stock.stocks.size()];
+	public static int[] popularpr() throws Exception {
+		int k[] = new int [Stock.getStocks().size()];
 		int id = 0;
-		for (int i = 0; i < Order.orders.size(); i++) {
-			for (int j = 0; j < Stock.stocks.size(); j++ ) {
-				if (Order.orders.get(i).idproduct1 == Stock.stocks.get(j).id) {
+		for (int i = 0; i < Order.getOrders().size(); i++) {
+			for (int j = 0; j < Stock.getStocks().size(); j++ ) {
+				if (Order.getOrders().get(i).idproduct1 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct2 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct3 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct4 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct5 == Stock.getStocks().get(j).id) {
 					k[j] = k[j] + 1;
 				}
 			}
@@ -57,42 +45,66 @@ public class Statistics {
 		for (int s = 0; s < k.length; s++) {
 			if (k[s] > max) {
 				max = k[s];
-				id = Stock.stocks.get(s).id;
+				id = s;
 			}
 		}
-		return id;
+		int array [] = new int[k.length];
+		for (int i = 0; i < k.length; i++) {
+			if(k[i] == max) {
+				array[i] = i;
+			}
+		}
+		return array;
 	}
 	
 	/* Λιγότερο δημοφιλές προϊόν */
-	public static int notpoppr() throws Exception {
+	public static int[] notpoppr() throws Exception {
 		int id = 0;
 		int min = 100000;
-		int k[] = new int [Stock.stocks.size()];
-		for (int i = 0; i < Order.orders.size(); i++) {
-			for (int j = 0; j < Stock.stocks.size(); j++ ) {
-				if (Order.orders.get(i).idproduct1 == Stock.stocks.get(j).id) {
+		int k[] = new int [Stock.getStocks().size()];
+		for (int i = 0; i < Order.getOrders().size(); i++) {
+			for (int j = 0; j < Stock.getStocks().size(); j++ ) {
+				if (Order.getOrders().get(i).idproduct1 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct2 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct3 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct4 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct5 == Stock.getStocks().get(j).id) {
 					k[j] = k[j] + 1;
 				}
 			}
+			
 		}
 		for (int i = 0; i < k.length; i++) {
 			if(k[i] < min) {
 				min = k[i];
-				id = Stock.stocks.get(i).id;
 			}
 		}
-		return id;
+		int array [] = new int[k.length];
+		for (int i = 0; i < k.length; i++) {
+			if(k[i] == min) {
+				array[i] = i;
+			}
+		}
+		return array;
 	}
 	
 	/* Προϊόν με τα μεγαλύτερα έσοδα */
 	
-	public static int maxpripr() throws Exception {
-		double a[] = new double[Stock.stocks.size()];
+	public static int[] maxpripr() throws Exception {
+		double a[] = new double[Stock.getStocks().size()];
 		int id = 0;
-		for (int i = 0; i < Order.orders.size(); i++) {
-			for (int j = 0; j < Stock.stocks.size(); j++ ) {
-				if (Order.orders.get(i).idproduct1 == Stock.stocks.get(j).id) {
-					a[j] = a[j] + Stock.stocks.get(j).price;
+		for (int i = 0; i < Order.getOrders().size(); i++) {
+			for (int j = 0; j < Stock.getStocks().size(); j++ ) {
+				if (Order.getOrders().get(i).idproduct1 == Stock.getStocks().get(j).id) {
+					a[j] = a[j] + Stock.getStocks().get(j).price;
 				}
 			}
 		}
@@ -100,20 +112,37 @@ public class Statistics {
 		for(int i = 0; i < a.length; i++) {
 			if(a[i] > max) {
 				max = a[i];
-				id = Stock.stocks.get(i).id;
 			}
 		}
-		return id;
+		int array [] = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			if(a[i] == max) {
+				array[i] = i;
+			}
+		}
+		return array;
 	}
 	
 	/* Προϊόν με τα μικρότερα έσοδα */
-	public static int minpripr() throws Exception {
+	public static int[] minpripr() throws Exception {
 		int id = 0;
-		double a[] = new double[Stock.stocks.size()];
-		for (int i = 0; i < Order.orders.size(); i++) {
-			for (int j = 0; j < Stock.stocks.size(); j++ ) {
-				if (Order.orders.get(i).idproduct1 == Stock.stocks.get(j).id) {
-					a[j] = a[j] + Stock.stocks.get(j).price;
+		double a[] = new double[Stock.getStocks().size()];
+		for (int i = 0; i < Order.getOrders().size(); i++) {
+			for (int j = 0; j < Stock.getStocks().size(); j++ ) {
+				if (Order.getOrders().get(i).idproduct1 == Stock.getStocks().get(j).id) {
+					a[j] = a[j] + Stock.getStocks().get(j).price;
+				}
+				if (Order.getOrders().get(i).idproduct2 == Stock.getStocks().get(j).id) {
+					a[j] = a[j] + Stock.getStocks().get(j).price;
+				}
+				if (Order.getOrders().get(i).idproduct3 == Stock.getStocks().get(j).id) {
+					a[j] = a[j] + Stock.getStocks().get(j).price;
+				}
+				if (Order.getOrders().get(i).idproduct4 == Stock.getStocks().get(j).id) {
+					a[j] = a[j] + Stock.getStocks().get(j).price;
+				}
+				if (Order.getOrders().get(i).idproduct5 == Stock.getStocks().get(j).id) {
+					a[j] = a[j] + Stock.getStocks().get(j).price;
 				}
 			}
 		}
@@ -121,34 +150,51 @@ public class Statistics {
 		for (int i = 0; i < a.length; i++) {
 			if(a[i] < min) {
 				min = a[i];
-				id = Stock.stocks.get(i).id;
 			}
 		}
-		return id;
+		int array [] = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			if(a[i] == min) {
+				array[i] = i;
+			}
+		}
+		return array;
 	}
 	/* ποσοστά προϊόντων στις παραγγελίες */
 	public static double[] average_pr_orders() throws Exception {
-		int k[] = new int [Stock.stocks.size()];
-		for (int i = 0; i < Order.orders.size(); i++) {
-			for (int j = 0; j < Stock.stocks.size(); j++ ) {
-				if (Order.orders.get(i).idproduct1 == Stock.stocks.get(j).id) {
+		int k[] = new int [Stock.getStocks().size()];
+		for (int i = 0; i < Order.getOrders().size(); i++) {
+			for (int j = 0; j < Stock.getStocks().size(); j++ ) {
+				if (Order.getOrders().get(i).idproduct1 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct2 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct3 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct4 == Stock.getStocks().get(j).id) {
+					k[j] = k[j] + 1;
+				}
+				if (Order.getOrders().get(i).idproduct5 == Stock.getStocks().get(j).id) {
 					k[j] = k[j] + 1;
 				}
 			}
 		}
-		double i[] = new double [Stock.stocks.size()];
-		for (int s = 0; s < Stock.stocks.size(); s++) {
-			i[s] = k[s]/Order.orders.size();
+		double i[] = new double [Stock.getStocks().size()];
+		for (int s = 0; s < Stock.getStocks().size(); s++) {
+			i[s] = k[s]/Order.getOrders().size();
 		}
 		return i;
 	}
 	
 	public static int popcustomer() throws Exception {
 		int idcust = 0;
-		int a[] = new int[Customer.customers.size()];
-		for (int i=0; i < Order.orders.size(); i++) {
-			for (int j = 0; j < Customer.customers.size(); j++) {
-				if (Order.orders.get(i).namecustomer == Customer.customers.get(j).namecustomer) {
+		int a[] = new int[Customer.getCustomers().size()];
+		for (int i=0; i < Order.getOrders().size(); i++) {
+			for (int j = 0; j < Customer.getCustomers().size(); j++) {
+				if (Order.getOrders().get(i).namecustomer == Customer.getCustomers().get(j).namecustomer) {
 					a[j] = a[j] + 1;
 				}
 			}
@@ -157,31 +203,31 @@ public class Statistics {
 		for (int s = 0; s < a.length; s++) {
 			if (a[s] > max) {
 				max = a[s];
-				idcust = Customer.customers.get(s).idcustomer;
+				idcust = Customer.getCustomers().get(s).idcustomer;
 			}
 		}
 		return idcust;
 	}
 	
-	/* Μέγιστη ποσότητα προϊόντων */
+	/* Μέγιστο απόθεμα */
 	public static int Highestquantity() throws Exception {
 		int highq = 0;
 		int id = 0;
-		for (int i = 0; i < Stock.stocks.size(); i++) {
-			if (Stock.stocks.get(i).stock > highq) {
-				highq = Stock.stocks.get(i).stock;
+		for (int i = 0; i < Stock.getStocks().size(); i++) {
+			if (Stock.getStocks().get(i).stock > highq) {
+				highq = Stock.getStocks().get(i).stock;
 			}
 		}
 		return highq;
 	}
 	
-	/* Ελάχιστη ποσότητα προϊόντων */
+	/* Ελάχιστo απόθεμα */
 	public static int Lowestquantity() throws Exception {
 		int lowq = 1000000000;
 		int id = 0;
-		for (int i = 0; i < Stock.stocks.size(); i++) {
-			if (Stock.stocks.get(i).stock < lowq) {
-				lowq= Stock.stocks.get(i).stock;
+		for (int i = 0; i < Stock.getStocks().size(); i++) {
+			if (Stock.getStocks().get(i).stock < lowq) {
+				lowq= Stock.getStocks().get(i).stock;
 			}
 		}
 		return lowq;
@@ -190,9 +236,9 @@ public class Statistics {
 	
 	public static double minprice() throws Exception {
 		double minp = 10000000;
-		for (int i=0; i < Stock.stocks.size(); i++) {
-			if (Stock.stocks.get(i).price < minp) {
-				minp = Stock.stocks.get(i).price;
+		for (int i=0; i < Stock.getStocks().size(); i++) {
+			if (Stock.getStocks().get(i).price < minp) {
+				minp = Stock.getStocks().get(i).price;
 			}
 		}
 		return minp;
@@ -200,12 +246,12 @@ public class Statistics {
 	
 	public static double maxprice() throws Exception {
 		double maxp = -1;
-		for (int i=0; i < Stock.stocks.size(); i++) {
-			if (Stock.stocks.get(i).price > maxp) {
-				maxp = Stock.stocks.get(i).price;
+		for (int i=0; i < Stock.getStocks().size(); i++) {
+			if (Stock.getStocks().get(i).price > maxp) {
+				maxp = Stock.getStocks().get(i).price;
 			}
 		}
 		return maxp;
 	}
-
+	
 }
